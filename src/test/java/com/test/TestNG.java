c9120b3a86f7766ee3988937effa446f058ce42c
Ramesh <rameshbabu.challa@gmail.com>
2022-02-19 06:50:28 +0530

    added new test method popuphandling1
    added new test method popuphandling1

--- src/test/java/com/test/TestNG.java
+++ src/test/java/com/test/TestNG.java
@@ -136,6 +136,56 @@ public class TestNG {
 	}
 	
 	
+	@Test(priority=0)
+	public void popUpWindowHandling1() throws IOException {
+		driver.navigate().to("https://demo.guru99.com/popup.php");
+		driver.manage().window().maximize();
+		
+		takeScreenshot(System.getProperty("user.dir") + "\\Results\\Screenshots_"+testRunTimeStamp+"\\ParentWindow.png");
+		
+		String parentWindow = driver.getWindowHandle();
+		System.out.println("parent window name:"+parentWindow);
+		driver.findElement(By.linkText("Click Here")).click();
+		
+		Set<String> allWindows = driver.getWindowHandles();
+		
+		Iterator<String> i1 = allWindows.iterator();
+		
+		while(i1.hasNext()) {
+			String childWindow = i1.next();
+			System.out.println("child window name:"+childWindow);
+			if(!parentWindow.equalsIgnoreCase(childWindow)) {
+				driver.switchTo().window(childWindow);
+				
+				takeScreenshot(System.getProperty("user.dir") + "\\Results\\Screenshots_"+testRunTimeStamp+"\\ChildWindow1.png");
+			
+				driver.findElement(By.name("emailid")).sendKeys("rameshbabu.challa@gmail.com");
+				driver.findElement(By.name("btnLogin")).click();
+				
+				takeScreenshot(System.getProperty("user.dir") + "\\Results\\Screenshots_"+testRunTimeStamp+"\\FilledDetails.png");
+				
+				driver.close();
+			} else {
+				System.out.println("inside else block");
+			}
+		}
+		
+		driver.switchTo().window(parentWindow);
+		takeScreenshot(System.getProperty("user.dir") + "\\Results\\Screenshots_"+testRunTimeStamp+"\\ParentWindow1.png");
+		driver.close();
+		
+		
+		//Step1. get current window and store in parentWindow variable
+		//Step2. get All windowHandles and store in iterator
+		//step3 : loop the iterator (2 windows)
+		//window 1: parent Window, widow 2: child window.
+		//step 4: loop the iterator. first value: parentwindow
+		//step 5: if(parentWindow.step1 parentWindow), if both are same then you dont need to switch.
+		//else you need to switch
+	
+		
+	}
+	
 	
 	
 	
